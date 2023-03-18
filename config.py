b077e7da2f7053f57e2193aa9e184f0fa9fc89d6
Daniel Phan <34669737+danielphan2003@users.noreply.github.com>
2023-03-19 00:16:27 +0700

    fixup(config.py): customize config

--- config.py
+++ config.py
@@ -5,38 +5,13 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 # The repository to add this issue to
-REPO_OWNER = 'e-tornike'
-REPO_NAME = 'arxiv-daily'
+REPO_OWNER = 'danielphan2003'
+REPO_NAME = 'arxiv-daily-summaries'
 
 # Set new submission url of subject
 NEW_SUB_URL = 'https://arxiv.org/list/cs/new'
 
 # Keywords to search
 KEYWORD_LIST = [
-    "extreme multi-label", 
-    "mixup", 
-    "data augmentation", 
-    "computational social science", 
-    "argument mining",
-    "scholarly",
-    "scholarly document processing", 
-    "citation context analysis",
-    "knowledge graph",
-    "legal text",
-    "legal",
-    "multi-task",
-    "robustness",
-    "summarization",
-    "knowledge discovery",
-    "cross-language information retrieval",
-    "cross-lingual information retrieval",
-    "text generation",
-    "passage generation",
-    "paraphrase",
-    "simplification",
-    "plagiarism",
-    "abstract meaning representation",
-    "semantic similarity",
-    "similarity measure",
-    "contrastive",
+    "iot"
 ]
