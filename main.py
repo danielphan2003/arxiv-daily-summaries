2aac18d1113ea9906b0fde03f3562482ed93b68f
Daniel Phan <34669737+danielphan2003@users.noreply.github.com>
2023-03-19 05:02:43 +0700

    feat(all): general refactor and improvements

--- main.py
+++ main.py
@@ -1,91 +1,114 @@
 # encoding: utf-8
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
+import requests
 
-import os
-
+from collections import namedtuple
 from bs4 import BeautifulSoup as bs
-import urllib.request
-from transformers import pipeline as pipe
+from deserialize import deserialize
+from github import Github
+from transformers import pipeline
+from typing import Dict, List, Optional
 
-from github_issue import make_github_issue
-from config import NEW_SUB_URL, KEYWORD_LIST
+Paper = namedtuple("Paper", ["id", "title", "authors", "main_page", "tldr", "comments", "pdf"])
 
+class Config:
+    arxiv_base: Optional[str] = "https://arxiv.org"
+    sub_url: Optional[str] = "https://arxiv.org/list/cs/new"
+    keywords: List[str]
+    tldr_max_length: Optional[int] = 100
+    model_name: Optional[str] = "facebook/bart-base"
 
-class Model:
-#     def __init__(self, model_path):
-    def __init__(self):
-        self.summarizer = pipe("summarization", model="facebook/bart-base")
+def sanitize_element(name: str, element: str) -> str:
+    return element.text.replace(f"{name}:", " ").replace("\n", "").strip()
 
-    def summarize(self, text: str):
-        return self.summarizer(text, max_length=100)
-
-
-def main():
-    model = Model()
+def get_subscription(config: Config) -> Dict[str, List[Paper]]:
+    summarizer = pipeline("summarization", config.model_name)
     
-    page = urllib.request.urlopen(NEW_SUB_URL)
-    soup = bs(page, "html.parser")
+    summarize = lambda text: summarize(text, max_length=config.tldr_max_length)
+
+    page = requests.get(config.sub_url)
+    soup = bs(page.data.decode('utf-8'), "html.parser")
     content = soup.body.find("div", {'id': 'content'})
 
     issue_title = content.find("h3").text
     dt_list = content.dl.find_all("dt")
     dd_list = content.dl.find_all("dd")
-    arxiv_base = "https://arxiv.org/abs/"
 
-    assert len(dt_list) == len(dd_list)
+    assert len(dt_list) == len(dd_list), "There should be a same amount of IDs compared to papers."
 
-    keyword_list = sorted(KEYWORD_LIST)
-    keyword_dict = {key: [] for key in keyword_list}
+    sub = {key: [] for key in config.keywords}
 
-    for i in range(len(dt_list)):
-        paper = {}
-        paper_number = dt_list[i].text.strip().split(" ")[2].split(":")[-1]
-        paper['main_page'] = arxiv_base + paper_number
-        paper['pdf'] = arxiv_base.replace('abs', 'pdf') + paper_number
+    for idx, dd in enumerate(dd_list):
+        abstract = sanitize_element("", dd.find("p", {"class": "mathjax"}))
 
-        paper['title'] = dd_list[i].find("div", {"class": "list-title mathjax"}).text.replace("Title: ", "").strip()
-        paper['authors'] = dd_list[i].find("div", {"class": "list-authors"}).text.replace("Authors:\n", "").replace(
-            "\n", "").strip()
-        paper['subjects'] = dd_list[i].find("div", {"class": "list-subjects"}).text.replace("Subjects: ", "").strip()
-        abstract = dd_list[i].find("p", {"class": "mathjax"}).text.replace("\n", " ").strip()
-        paper['tldr'] = model.summarize(abstract)[0]["summary_text"]
-        #paper['abstract'] = dd_list[i].find("p", {"class": "mathjax"}).text.replace("\n", " ").strip()
-
-        for keyword in keyword_list:
+        for keyword in config.keywords:
             if keyword.lower() in abstract.lower():
-                keyword_dict[keyword].append(paper)
+                id = dt_list[i].text.strip().split(" ")[2].split(":")[-1]
+                comments = dd.find("div", {"class": "list-comments"}
 
-    full_report = ''
-    for keyword in keyword_list:
-        # full_report = full_report + '## Keyword: ' + keyword + '\n'
-        #full_report = full_report + '<h2>Keyword: ' + keyword + '</h2>'
+                paper = Paper(
+                    id,
+                    title = sanitize_element("Title", dd.find("div", {"class": "list-title mathjax"}))
+                    authors = sanitize_element("Authors", dd.find("div", {"class": "list-authors"}))
+                    main_page = f"{arxiv_base}/abs/{id}",
+                    tldr = summarize(abstract)[0]["summary_text"],
+                    comments = comments ? sanitize_element("Comments", comments) : None,
+                    pdf = f"{arxiv_base}/pdf/{id}",
+                    # abstract = abstract,
+                )
 
-        if len(keyword_dict[keyword]) == 0:
-            #full_report = full_report + 'There is no result <br>'
+                sub[keyword].append(paper)
+
+    return sub
+
+def generate_full_report(config: Config, sub: Dict[str, List[Paper]]) -> str:
+    full_report = ""
+
+    for keyword in config.keywords:
+        paper_count = len(sub[keyword])
+
+        if paper_count == 0:
             pass
+ 
+        full_report += f"<h2>Keyword: {keyword} ({paper_count} papers)</h2>"
+ 
+        full_report += "<details>"
+ 
+        for paper in sub[keyword]:
+            # report = '### {}\n - **Authors:** {}\n - **Subjects:** {}\n - **Arxiv link:** {}\n - **Pdf link:** {}\n - **Abstract**\n {}' \
+            #     .format(paper['title'], paper['authors'], paper['subjects'], paper['main_page'], paper['pdf'],
+            #             paper['abstract'])
+            report = f"<h3>{paper.title}</h3>\
+                <strong>:brain: Authors:</strong> {paper.authors}<br>\
+                <strong>:paw_prints: Details:</strong> <a href='{paper.main_page}'>arXiv:{paper.id}</a><br>\
+                <strong>:ramen: tl;dr:</strong> {paper.tldr}...</a><br>\
+                {paper.comments ? f"<strong>:sunflower: Comments:</strong> {paper.comments}<br>" : ""}\
+                <a href='{paper.pdf}'>:seedling: Read more &#8594;</a><br>"
+ 
+            full_report += report
+
+        full_report += "</details>"
+
+    return full_report
+                                   
+def main():
+    with open("config.json", "r", encoding="utf-8") as f:
+        config = deserialize(Config, f)
+
+    sub = get_subscription(config)
+
+    full_report = generate_full_report(config, sub)
+
+    # only make issue if at least one keyword has a new paper
+    if full_report:
+        # and if is running in GitHub Actions
+        if os.environ['GITHUB_ACTIONS'] and os.environ['GITHUB_ACTIONS'] == "true":
+            g = Github(os.environ['GITHUB_TOKEN'])
+            user = os.environ['GITHUB_REPOSITORY_OWNER']
+            repo = g.get_repo(os.environ['GITHUB_REPOSITORY'])
+            issue = repo.create_issue(title=issue_title, body=full_report, assignee=user, labels=config.keywords)
         else:
-            full_report = full_report + '<h2>Keyword: ' + keyword + '</h2>'
-            full_report = full_report + "<details>"
-            for paper in keyword_dict[keyword]:
-                # report = '### {}\n - **Authors:** {}\n - **Subjects:** {}\n - **Arxiv link:** {}\n - **Pdf link:** {}\n - **Abstract**\n {}' \
-                #     .format(paper['title'], paper['authors'], paper['subjects'], paper['main_page'], paper['pdf'],
-                #             paper['abstract'])
-                report = f"<h3>{paper['title']}</h3>\
-                    <strong>Authors:</strong> {paper['authors']}<br>\
-                    <strong>Arxiv:</strong> <a href='{paper['main_page']}'>{paper['main_page']}</a><br>\
-                    <strong>TLDR:</strong> {paper['tldr']}"
-                full_report = full_report + report + '<br>'
-            full_report = full_report + "</details>"
-
-    if full_report != '' and len(full_report) > 0:  # only make issue if at least one keyword has a new paper
-        # Authentication for user filing issue (must have read/write access to repository to add issue to)
-        if 'GITHUB' in os.environ:
-            USERNAME, TOKEN = os.environ['GITHUB'].split(',')
-        make_github_issue(title=issue_title, body=full_report, assignee=USERNAME, TOKEN=TOKEN, labels=keyword_list)
-
+            print(issue_title)
+            print(full_report)
 
 if __name__ == '__main__':
     main()
